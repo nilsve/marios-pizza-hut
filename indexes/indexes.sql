49cb34d54845c3ec4779e2bf54ded6cd74bc9568
Indy Maat <indymaat@gmail.com>
2018-11-01 19:36:33 +0100

    Indexes op postcode- en filiaaltabel

---
+++ indexes/indexes.sql
@@ -0,0 +1,8 @@
+create index idx_postcode_postcode     on postcode (postcode);
+create index idx_postcode_reeks        on postcode (reeks);
+create index idx_postcode_eerste_huis  on postcode (eerste_huis);
+create index idx_postcode_laatste_huis on postcode (laatste_huis);
+create index idx_postcode_woonplaats   on postcode (woonplaats);
+create index idx_postcode_straatnaam   on postcode (straatnaam);
+create index idx_filiaal_naam          on filiaal (naam);
+
